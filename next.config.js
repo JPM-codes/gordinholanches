b2b9d5b0aa3d5b59ab31e3f74c11f9708daeaa9c
JPM-codes <mellojoaopedro20@gmail.com>
2026-07-30 09:50:36 -0300

    chore: update .gitignore, remove unused zip file, and enhance tsconfig.json structure

--- next.config.js
+++ next.config.js
@@ -2,12 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       { protocol: "https", hostname: "**" }
     ]
   },
   compress: true,
+  output: 'export',
+  distDir: 'dist'
 };
 
 module.exports = nextConfig;
